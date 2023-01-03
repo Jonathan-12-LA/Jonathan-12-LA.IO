0655ffac5254c084973aaa6d8c00b64de8b30538
Jonathan Larin <114048404+Jonathan-12-LA@users.noreply.github.com>
2023-01-03 12:49:02 -0600

    commit
    solution modulo4

---
+++ mod4_solution/assignment4/assignment4-solution-starter/easier/script.js
@@ -0,0 +1,42 @@
+
+(function sayhello() {
+
+var names =new Array();
+names[0]="yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="paula";
+names[8]="Laura";
+names[9]="Jim";
+
+
+
+for (var i = 0; i < names.length; i++) {
+console.log(names[i])
+  
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+ 
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
+
+
+// DOM manipulation
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
+
+
+
+  
+
+  
